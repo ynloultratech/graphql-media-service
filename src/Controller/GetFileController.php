5ce4a2c47d1600bb88eba7246ba3479aa7cdbdd9
Rafael Santos <raf.rsr@gmail.com>
2019-12-16 14:46:49 -0500

    fix invalid field name when try to get a private file using invalid path

--- src/Controller/GetFileController.php
+++ src/Controller/GetFileController.php
@@ -39,34 +39,39 @@ class GetFileController extends Controller
     {
         //api context is not possible here because its a action out of api
         $class = $this->container->getParameter('media_service_config')['class'];
-        $media = $this->container->get('doctrine')->getRepository($class)->findOneBy($request->get('_route_params'));
-        if ($media instanceof FileInterface) {
-            $provider = $this->getStorageProvider($media->getStorage());
+        $id = $request->get('_route_params')['id'] ?? null;
+        $name = $request->get('_route_params')['name'] ?? null;
 
-            if (($provider instanceof LocalMediaStorageProvider) && $provider->isValidSignedRequest($media, $request)) {
-                $fileToDownload = new File(
-                    $provider->getFileName($media)
-                );
+        if ($id && $name) {
+            $media = $this->container->get('doctrine')->getRepository($class)->findOneBy(['id' => $id, 'name' => $name]);
+            if ($media instanceof FileInterface) {
+                $provider = $this->getStorageProvider($media->getStorage());
 
-                /** @var MediaServerExtensionInterface $extension */
-                foreach ($this->extensions as $extension) {
-                    $newFileToDownload = $extension->preDownload($provider, $media, $request);
-                    if ($newFileToDownload) {
-                        $fileToDownload = $newFileToDownload;
+                if (($provider instanceof LocalMediaStorageProvider) && $provider->isValidSignedRequest($media, $request)) {
+                    $fileToDownload = new File(
+                        $provider->getFileName($media)
+                    );
+
+                    /** @var MediaServerExtensionInterface $extension */
+                    foreach ($this->extensions as $extension) {
+                        $newFileToDownload = $extension->preDownload($provider, $media, $request);
+                        if ($newFileToDownload) {
+                            $fileToDownload = $newFileToDownload;
+                        }
                     }
+
+                    return new BinaryFileResponse(
+                        $fileToDownload,
+                        200,
+                        [
+                            'Content-Type' => $media->getContentType(),
+                            'Content-Length' => $media->getSize(),
+                        ]
+                    );
                 }
 
-                return new BinaryFileResponse(
-                    $fileToDownload,
-                    200,
-                    [
-                        'Content-Type' => $media->getContentType(),
-                        'Content-Length' => $media->getSize(),
-                    ]
-                );
+                return new Response(null, Response::HTTP_FORBIDDEN);
             }
-
-            return new Response(null, Response::HTTP_FORBIDDEN);
         }
 
         return new Response(null, Response::HTTP_NOT_FOUND);
